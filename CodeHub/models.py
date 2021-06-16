12090ed9061fc7b87a33e7274682eb788afe99db
Suryansh <suryanshsingh.civ18@iitbhu.ac.in>
2021-06-17 00:06:59 +0530

    First Commit

---
+++ CodeHub/models.py
@@ -0,0 +1,11 @@
+from django.conf import settings
+from django.db import models
+class Question(models.Model):
+    author=models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
+    content=models.TextField()
+    added_time=models.DateTimeField()
+class Answer(models.Model):
+    author=models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
+    content=models.TextField()
+    link_to_ques=models.ForeignKey(Question,on_delete=models.CASCADE)
+    added_time=models.DateTimeField()
