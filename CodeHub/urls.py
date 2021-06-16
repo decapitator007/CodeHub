e04135a3bdc7ee76d40dd746a99d6e3ac76ea3b5
Suryansh <suryanshsingh.civ18@iitbhu.ac.in>
2021-06-17 00:53:41 +0530

    HTML

---
+++ CodeHub/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+urlpatterns=[
+    path('',views.home,name='home'),
+    path('identify/',views.identify,name='identify'),
+    path('newques/',views.new_ques,name='new_ques'),
+    path('question/<int:pk>/',views.ques_detail,name='ques_detail'),
+    path('question/<int:pk>/ans/',views.add_ans,name='add_ans'),
+    path('question/<int:pk>/edit/<int:ak>',views.edit_ans,name='edit_ans'),
+    path('question/<int:pk>/delete/<int:ak>',views.delete_ans,name='delete_ans'),
+    path('out/',views.out,name='out'),
+    path('register/',views.register,name='register'),
+]
--- CodeHub/views.py
+++ CodeHub/views.py
@@ -1,3 +1,135 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import render,get_object_or_404,redirect
+from django.http import HttpResponse
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,login,logout
+from .forms import signin,signup,QForm,AForm
+from .models import Question,Answer
+from django.contrib import messages
+from django.conf import settings
+from django.core.mail import send_mail
+def delete_ans(request,pk,ak):
+    answer=get_object_or_404(Answer,pk=ak)
+    if request.user.is_authenticated and request.user==answer.author:
+        if request.method=="POST":
+            if 'yes' in request.POST:
+                answer.delete()
+            return redirect('ques_detail',pk=pk)
+        else:
+            return render(request,'CodeHub/delete_ans.html',{})
+    else:
+        return redirect('home')
+def edit_ans(request,pk,ak):
+    answer=get_object_or_404(Answer,pk=ak)
+    if request.user.is_authenticated and request.user==answer.author:
+        if request.method=='POST':
+            form=AForm(request.POST,instance=answer)
+            if form.is_valid():
+                answer=form.save(commit=False)
+                answer.added_time=timezone.now()
+                answer.save()
+                messages.success(request,'Answer edited!')
+                return redirect('ques_detail',pk=pk)
+        else:
+            form=AForm(instance=answer)
+        return render(request,'CodeHub/add_ans.html',{'form':form})
+    else:
+        return redirect('home')
+def ques_detail(request,pk):
+    question=get_object_or_404(Question,pk=pk)
+    answers=Answer.objects.filter(link_to_ques=question).order_by('-added_time')
+    return render(request,'CodeHub/ques_detail.html',{'question':question,'answers':answers})
+def new_ques(request):
+    if request.user.is_authenticated:
+        if request.method=='POST':
+            form=QForm(request.POST)
+            if form.is_valid():
+                question=form.save(commit=False)
+                question.author=request.user
+                question.added_time=timezone.now()
+                question.save()
+                messages.success(request,'Your question has been added!')
+                return redirect('home')
+        else:
+            form=QForm()
+        return render(request,'CodeHub/new_ques.html',{'form':form})
+    else:
+        messages.warning(request,'You must login to ask a question!')
+        return redirect('identify')
+def add_ans(request,pk):
+    question=get_object_or_404(Question,pk=pk)
+    if request.user.is_authenticated:
+        if request.method=='POST':
+            form=AForm(request.POST)
+            if form.is_valid():
+                answer=form.save(commit=False)
+                answer.author=request.user
+                answer.added_time=timezone.now()
+                answer.link_to_ques=question
+                answer.save()
+                messages.success(request,'Your answer has been added!')
+                return redirect('ques_detail',pk=pk)
+        else:
+            form=AForm()
+        return render(request,'CodeHub/add_ans.html',{'form':form})
+    else:
+        messages.warning(request,'You must login to answer a question!')
+        return redirect('identify')
+def home(request):
+    questions=Question.objects.order_by('-added_time')
+    return render(request,'CodeHub/home.html',{'questions':questions})
+def identify(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        if request.method=='POST':
+            form=signin(request.POST)
+            if form.is_valid():
+                username=form.cleaned_data.get("username")
+                password=form.cleaned_data.get("password")
+                user=authenticate(request,username=username,password=password)
+                if user is not None:
+                    login(request,user)
+                    messages.success(request,'Logged in!')
+                    return redirect('home')
+                else:
+                    messages.warning(request,'Invalid Credentials!')
+                    return render(request,'CodeHub/identify.html',{'form':form})
+        else:
+            form=signin()
+        return render(request,'CodeHub/identify.html',{'form':form})
+def register(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        if request.method=='POST':
+            form=signup(request.POST)
+            if form.is_valid():
+                firstname=form.cleaned_data.get("firstname")
+                lastname=form.cleaned_data.get("lastname")
+                username=form.cleaned_data.get("username")
+                email=form.cleaned_data.get("email")
+                password=form.cleaned_data.get("password")
+                cpassword=form.cleaned_data.get("cpassword")
+                error=""
+                if User.objects.filter(username=username).exists():
+                    error="This username is already registered!"
+                elif User.objects.filter(email=email).exists():
+                    error="This email is already registered!"
+                elif password!=cpassword:
+                    error="Passwords did not match!"
+                if error!="":
+                    messages.warning(request,error)
+                    return render(request,'CodeHub/register.html',{'form':form})
+                User.objects.create_user(first_name=firstname,last_name=lastname,username=username,email=email,password=password)
+                messages.success(request,'Account created successfully!')
+                send_mail('Welcome to CodeHub','Hi '+firstname+'! Thank you for registering on CodeHub.',settings.EMAIL_HOST_USER,[email])
+                return redirect('identify')
+        else:
+            form=signup()
+        return render(request,'CodeHub/register.html',{'form':form})
+def out(request):
+    if request.user.is_authenticated:
+        logout(request)
+        messages.success(request,'Logged out!')
+    return redirect('identify')
